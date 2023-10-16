dfc3aac691e1a155097836efa486eef809ecceb3
Muhamed Fejzic <91850725+mfejzic@users.noreply.github.com>
2023-10-16 17:11:19 -0500

    Update main.tf

--- Cloudfront/main.tf
+++ Cloudfront/main.tf
@@ -1,29 +1,39 @@
-
+# Create S3 bucket
 resource "aws_s3_bucket" "site_origin" {
   bucket = "host-website-mf37"
 }
+
+# Allow public access to bucket objects
 resource "aws_s3_bucket_public_access_block" "site_origin" {
   bucket                  = aws_s3_bucket.site_origin.bucket
-  block_public_acls       = false //prevent the creation or modification of public ACLs on objects within the bucket
-  block_public_policy     = false //Whether Amazon S3 should block public bucket policies for this bucket
-  ignore_public_acls      = false //Whether Amazon S3 should ignore public ACLs for this bucket
-  restrict_public_buckets = false // Whether Amazon S3 should restrict public bucket policies for this bucket.
+  block_public_acls       = false 
+  block_public_policy     = false 
+  ignore_public_acls      = false 
+  restrict_public_buckets = false 
 }
+
+# Encrypt bucket objects
 resource "aws_s3_bucket_server_side_encryption_configuration" "site_origin" {
   bucket = aws_s3_bucket.site_origin.bucket
+  # Use AES256 encryption for each object
   rule {
-    apply_server_side_encryption_by_default { // protect data at rest
+    apply_server_side_encryption_by_default {
       sse_algorithm = "AES256"
     }
   }
 }
+
+# Enable bucket versioning for accidental deletion
 resource "aws_s3_bucket_versioning" "site_origin" {
   bucket = aws_s3_bucket.site_origin.bucket
-  versioning_configuration { // prevent accidental deletion
+  versioning_configuration { 
     status = "Enabled"
   }
 }
-resource "aws_s3_object" "content" { // create object after creating bucket
+
+# Add html file to bucket
+resource "aws_s3_object" "content" { 
+  # Create object after creating bucket
   depends_on = [
     aws_s3_bucket.site_origin
   ]
@@ -31,34 +41,53 @@ resource "aws_s3_object" "content" { // create object after creating bucket
   key                    = "index.html"
   source                 = "index.html"
   server_side_encryption = "AES256"
-  content_type           = "text/html" // specify content type
+  content_type           = "text/html" 
 }
 
+
+# Add jpg folder inside bucket/ upload jpg files using commands in terminal
+resource "aws_s3_object" "jpg" { // create object after creating bucket
+  depends_on = [
+    aws_s3_bucket.site_origin
+  ]
+  bucket                 = aws_s3_bucket.site_origin.bucket
+  key                    = "jpg/"
+  //source                 = "jpg/"
+  server_side_encryption = "AES256"
+  content_type           = "image/jpeg" 
+}
+
+# Use existing acm certifacte from the AWS console
 data "aws_acm_certificate" "amazon_issued" {
   domain      = var.subdomain_name
   types       = ["AMAZON_ISSUED"]
   most_recent = true
 }
 
+# Create origin access identity/ point cloudfront to s3 to serve static web pages
 resource "aws_cloudfront_origin_access_control" "site_access" {
   name                              = "security_pillar100_cf_s3_oac"
   origin_access_control_origin_type = "s3"
-  signing_behavior                  = "always" // refers to process of generating signed urls to control access to content
-  signing_protocol                  = "sigv4"  // procedures that govern the use/generation of signed urls and cookies/ specifies format and content of access policy/algorithms for signing
+  signing_behavior                  = "always" 
+  signing_protocol                  = "sigv4"  
 }
+
+# Create cloudfront to serve static webpages
 resource "aws_cloudfront_distribution" "site_access" {
-  depends_on = [ // resource depends on the creation of your s3 bucket
+  # Resource depends on the creation of your S3 bucket
+  depends_on = [ 
     aws_s3_bucket.site_origin,
     aws_cloudfront_origin_access_control.site_access,
   ]
-  enabled             = true         // states distribution is enabled upon creation 
-  default_root_object = "index.html" // tells distribution which page to load first 
+  enabled             = true      
+  default_root_object = "index.html" 
 
-  default_cache_behavior { // describes how we want cloudfront to fetch objects from s3
+  # Describes how we want cloudfront to fetch objects from s3/ redirect http traffic to https
+  default_cache_behavior { 
     allowed_methods        = ["GET", "HEAD"]
     cached_methods         = ["GET", "HEAD"]
     target_origin_id       = aws_s3_bucket.site_origin.id
-    viewer_protocol_policy = "redirect-to-https" //redirect http traffic to https
+    viewer_protocol_policy = "redirect-to-https" 
     min_ttl                = 10
     default_ttl            = 30
     max_ttl                = 60
@@ -70,18 +99,22 @@ resource "aws_cloudfront_distribution" "site_access" {
       }
     }
   }
-  aliases = [var.domain_name, var.subdomain_name] //missing
+  aliases = [var.domain_name, var.subdomain_name] 
   origin {
     domain_name              = aws_s3_bucket.site_origin.bucket_regional_domain_name
     origin_id                = aws_s3_bucket.site_origin.id
     origin_access_control_id = aws_cloudfront_origin_access_control.site_access.id
   }
+
+  # Allow access only from USA and Canada
   restrictions {
     geo_restriction {
       restriction_type = "whitelist"
       locations        = ["US", "CA"]
     }
   }
+
+  # Add tls certificate from acm console
   viewer_certificate {
     acm_certificate_arn = data.aws_acm_certificate.amazon_issued.id
     ssl_support_method  = "sni-only"
@@ -91,6 +124,7 @@ resource "aws_cloudfront_distribution" "site_access" {
   }
 }
 
+# Create policy to allow cloudfront to fetch html file S3 in order to serve it 
 resource "aws_s3_bucket_policy" "site_origin" {
   bucket = aws_s3_bucket.site_origin.id
   policy = jsonencode({
@@ -107,18 +141,21 @@ resource "aws_s3_bucket_policy" "site_origin" {
   })
 }
 
+# Create route 53 to route traffic to cloudfront/ edge locations
 data "aws_route53_zone" "hosted_zone" {
   name         = var.domain_name
   private_zone = false
 }
+
+# Create records with failover policy
 resource "aws_route53_record" "primary_alias" {
   zone_id = data.aws_route53_zone.hosted_zone.zone_id
   name    = "www.${data.aws_route53_zone.hosted_zone.name}"
   type    = "A"
 
   alias {
-    name                   = aws_cloudfront_distribution.site_access.domain_name    // refers to distribution domain name after new cf created
-    zone_id                = aws_cloudfront_distribution.site_access.hosted_zone_id // Fixed value for CloudFront distribution
+    name                   = aws_cloudfront_distribution.site_access.domain_name    
+    zone_id                = aws_cloudfront_distribution.site_access.hosted_zone_id
     evaluate_target_health = true
   }
   failover_routing_policy {
@@ -133,8 +170,8 @@ resource "aws_route53_record" "secondary_alias" {
   type    = "A"
   
   alias {
-    name                   = aws_cloudfront_distribution.site_access.domain_name    // refers to distribution domain name after new cf created
-    zone_id                = aws_cloudfront_distribution.site_access.hosted_zone_id // Fixed value for CloudFront distribution
+    name                   = aws_cloudfront_distribution.site_access.domain_name   
+    zone_id                = aws_cloudfront_distribution.site_access.hosted_zone_id 
     evaluate_target_health = true
   }
   failover_routing_policy {
@@ -144,6 +181,7 @@ resource "aws_route53_record" "secondary_alias" {
   health_check_id = aws_route53_health_check.secondary.id
 }
 
+# Create health checks for both records / if primary record fails, traffic will route to secondary record 
 resource "aws_route53_health_check" "primary" {
   fqdn              = "www.${data.aws_route53_zone.hosted_zone.name}"
   port              = 443
@@ -154,7 +192,6 @@ resource "aws_route53_health_check" "primary" {
     Name = "primary_health_check"
   }
 }
-
 resource "aws_route53_health_check" "secondary" {
   fqdn              = "www.${data.aws_route53_zone.hosted_zone.name}"
   port              = 443
@@ -165,73 +202,3 @@ resource "aws_route53_health_check" "secondary" {
     Name = "secondary_health_check"
   }
 }
-
-
-
-# resource "aws_route53_health_check" "health" {
-#   fqdn              = "www.fejzic37.com"
-#   port              = 80
-#   type              = "HTTP"
-#   resource_path     = "index.html"
-#   failure_threshold = "5"
-#   request_interval  = "30"
-
-#   tags = {
-#     Name = "tf-test-health-check"
-#   }
-# }
-
-/*
-
-
-resource "aws_s3_bucket_policy" "site_origin" {
-  depends_on = [
-    data.aws_iam_policy_document.site_origin
-  ]
-  bucket = aws_s3_bucket.site_origin.id
-  policy = data.aws_iam_policy_document.site_origin.json
-}
-
-data "aws_iam_policy_document" "site_origin" {
-  depends_on = [
-    aws_cloudfront_distribution.site_access,
-    aws_s3_bucket.site_origin
-  ]
-  statement {
-    sid     = "s3_cloudfront_static_website"
-    effect  = "Allow"
-    actions = ["s3:GetObject"]
-    principals {
-      type        = "Service"
-      identifiers = ["cloudfront.amazonaws.com"]
-    }
-    resources = ["arn:aws:s3:::${aws_s3_bucket.site_origin.bucket}/*"]
-    condition {
-      test     = "StringEquals"
-      variable = "AWS:SourceARN"
-      values   = [aws_cloudfront_distribution.site_access.id]
-    }
-  }
-}
-
-
-
-
-resource "aws_s3_bucket_policy" "site_origin" {
-  bucket = aws_s3_bucket.site_origin.id
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [{
-      "Sid" : "PublicReadGetObject",
-      "Effect" : "Allow",
-      "Principal" : {
-          "Service" : "cloudfront.amazonaws.com"
-        },
-      "Action" : "s3:GetObject",
-      "Resource" : "arn:aws:s3:::${aws_s3_bucket.site_origin.id}/*"
-    }]
-  })
-}
-
-
-*/
